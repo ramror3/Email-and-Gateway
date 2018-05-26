5d5a6a6b822684c99cf35efe9b91e66b5dde1170
ramror <ramesh.ramror@gmail.com>
2018-05-26 14:00:20 +0530

    Setup email functionality

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+	:address => 'smtp.sendgrid.net',
+	:port => '587',
+	:authentication => 'plain',
+	:username => ENV['Ramesh3'],
+	:password => ENV['R@meshRams3'],
+	:domain => 'heroku.com',
+	:enable_starttls_auto => true
+}
